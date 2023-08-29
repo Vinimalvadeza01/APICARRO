141b732335d6b859a2cd65a4a956cbf95e1fb25f
Vinicius <Vvini3881@gmail.com>
2023-08-29 11:56:58 -0300

    primeiro commit

---
+++ API/SRC/repository/connection.js
@@ -0,0 +1,9 @@
+import mysql2 from 'mysql2/promise'
+
+let con = await mysql2.createConnection({
+    host :process.env.HOST,
+    database: process.env.DB,
+    user:process.env.USER,
+    password: process.env.PWD,
+});
+
