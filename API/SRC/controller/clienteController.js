673eef400fd4021f567c341a22e41dea178f4886
Vinicius <vvini3881@gmail.com>
2023-08-30 09:48:09 -0300

    ook

---
+++ API/SRC/controller/clienteController.js
@@ -0,0 +1,4 @@
+import {inserir, consultar, alterar, deletar} from '../repository/clienteRepository.js';
+
+
+import { Router } from "express";
--- API/SRC/index.js
+++ API/SRC/index.js
@@ -1,11 +1,15 @@
-import 'dotenv/config';
-import express from 'express';
+import 'dotenv/config'
+import express from 'express'
 import cors from 'cors'
 
-const server =express();
-server.use (cors())
-server.use(express.json());
+import veiculoController from './controller/veiculoController.js'
 
-server.listen(
-    process.env.PORT,
-    () =>console.log( `API subiu na porta ${process.env.PORT}`));
+let servidor = express();
+servidor.use(cors());
+servidor.use(express.json());
+
+servidor.use(veiculoController);
+
+
+servidor.listen(process.env.PORT,
+                () => console.log('API SUBIUUUUU!'));
